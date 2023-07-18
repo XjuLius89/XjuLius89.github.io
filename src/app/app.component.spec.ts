19d2f4c29b5fb06ea5154507c0318c131adcde7b
TPattara <ptr.tnkjnt@gmail.com>
2023-07-18 22:00:21 +0700

    test1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'default'`, () => {
+  it(`should have as title 'xjulius89.github.io'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('default');
+    expect(app.title).toEqual('xjulius89.github.io');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('default app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('xjulius89.github.io app is running!');
   });
 });
